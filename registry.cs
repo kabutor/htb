8711ccb17c315770eb9fc83470fd5021293c01b5
kabutor <43006263+kabutor@users.noreply.github.com>
2020-09-05 15:20:38 +0200

    Add files via upload
    Show permissions on a HKLM registry key, 
    Compile with csc.exe

---
+++ registry.cs
@@ -0,0 +1,43 @@
+// COMPILE AS
+// c:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe -out:registry.exe registry.cs
+// USE AS
+// registry SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Run
+
+using System;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Security;
+using Microsoft.Win32;
+using System.Linq;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+        if (args.Length < 1)
+        {
+            Console.WriteLine("No Args");
+            System.Environment.Exit(1);
+        }
+        else
+        {
+            Console.WriteLine(args[0]);
+        }
+        using (RegistryKey key = Registry.LocalMachine.OpenSubKey(args[0]))
+	    {
+        var security = key.GetAccessControl();
+
+
+        foreach (RegistryAccessRule rule in security.GetAccessRules(true, true, typeof(System.Security.Principal.NTAccount)))
+            {
+                Console.WriteLine("        User: {0}", rule.IdentityReference);
+                Console.WriteLine("        Type: {0}", rule.AccessControlType);
+                Console.WriteLine("      Rights: {0}", rule.RegistryRights);
+                //Console.WriteLine(" Inheritance: {0}", rule.InheritanceFlags);
+                //Console.WriteLine(" Propagation: {0}", rule.PropagationFlags);
+                //Console.WriteLine("   Inherited? {0}", rule.IsInherited);
+                Console.WriteLine();
+            }
+        }
+	}
+}
